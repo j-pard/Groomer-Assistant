14731c93fd8080de612349bf58864ff04ff901cc
J-Pard <pardons.jonathan@gmail.com>
2021-12-14 20:11:46 +0100

    Back url

--- resources/views/manager/appointments/form.blade.php
+++ resources/views/manager/appointments/form.blade.php
@@ -1,17 +1,10 @@
 @extends('manager.layouts.app')
 
 @section('content')
-    @if (Route::currentRouteName() == 'pets.appointment')
-        <livewire:appointments.header 
+    <livewire:appointments.header 
         :model="$appointment"
-        backUrl="{{ route('pets.edit', ['pet' => $pet]) }}"
+        backUrl="{{ url()->previous() }}"
     />
-    @else
-        <livewire:appointments.header 
-            :model="$appointment"
-            backUrl="{{ isset($customer) ? route('customers.edit', ['customer' => $customer]) : route('customers.index') }}"
-        />
-    @endif
 
     <livewire:appointments.form :customer="$customer" :pet="$pet" :appointment="$appointment" />
 @endsection
