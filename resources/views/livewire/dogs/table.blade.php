84576d17ed9c438632dffe5a2872f768db41206c
J-Pard <pardons.jonathan@gmail.com>
2023-09-25 10:19:50 +0200

    List details in desktop & mobile

--- resources/views/livewire/dogs/table.blade.php
+++ resources/views/livewire/dogs/table.blade.php
@@ -14,24 +14,33 @@
     <div class="d-flex flex-column pt-3 pb-1 mb-1">
         @foreach ($dogs as $dog)
             <div class="list-element mx-2 my-1 py-2 px-1 px-sm-3" data-id="{{ $dog->id }}">
-                <div class="d-flex flex-row">
+                <div class="d-flex flex-row col-md-5">
                     <div class="d-flex flex-row align-items-center">
                         <div class="avatar mx-2 mx-sm-4">{{ $dog->avatar }}</div>
                     </div>
                     <div class="d-flex flex-column">
                         <div>{{ $dog->name }}</div>
                         <span class="text--quartz">{{ $dog->owner_name }}</span>
-                        <em class="text--quartz">{{ $dog->owner_phone }}</em>
+                        <em class="text--quartz cell-mobile">{{ $dog->owner_phone }}</em>
                     </div>
                 </div>
-                <div class="d-flex flex-column text-end justify-content-between">
-                    <div>{{ $dog->mainBreed->breed }}</div>
+
+                <div class="cell-desktop col-md-3">
+                    <div class="d-flex flex-column">
+                        <div class="text--quartz"><i class="fa-solid fa-phone text--copper me-2"></i>{{ $dog->owner_phone }}</div>
+                        <div class="text--quartz"><i class="fa-solid fa-calendar-day text--copper me-2"></i>{{ $dog->updated_at->format('d-m-Y') }}</div>
+                    </div>
+                </div>
+
+                <div class="d-flex flex-column text-end justify-content-between col-md-4">
+                    <div>{{ $dog->mainBreed->breed . ($dog->second_breed_id !== null ? '  /  ' . $dog->secondBreed->breed : '') }}</div>
                     <div class="d-flex justify-content-end flex-no-wrap">
                         @if ($dog->second_breed_id !== null)
                             <span class="badge rounded-pill bg--quartz me-2">Croisé</span>
                         @endif
                         <span class="badge rounded-pill {{ 'bg-' . App\Enums\DogStatus::getColor($dog->status) }}">{{ App\Enums\DogStatus::getText($dog->status) }}</span>
                     </div>
+                    <div><em class="text--quartz cell-mobile">{{ $dog->updated_at->format('d-m-Y') }}</em></div>
                 </div>
             </div>
         @endforeach
