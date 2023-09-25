03ac86298ac539125d0eed4085c3e95c93020e03
J-Pard <pardons.jonathan@gmail.com>
2023-09-25 13:31:23 +0200

    Dogs > Loading state

--- resources/views/livewire/dogs/table.blade.php
+++ resources/views/livewire/dogs/table.blade.php
@@ -11,7 +11,7 @@
         </div>
     </div>
 
-    <div class="d-flex flex-column pt-3 pb-1 mb-1">
+    <div class="d-flex flex-column pt-3 pb-1 mb-1" wire:loading.class="opacity-50">
         @foreach ($dogs as $dog)
             <div class="list-element mx-2 my-1 py-2 px-1 px-sm-3" data-id="{{ $dog->id }}">
                 <div class="d-flex flex-row col-md-5">
