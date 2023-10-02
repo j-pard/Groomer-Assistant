c14e16598eef221a32c3883cca51059906a5c46e
J-Pard <pardons.jonathan@gmail.com>
2023-10-02 13:54:37 +0200

    Improve nav behavior

--- public/js/app.js
+++ public/js/app.js
@@ -5215,24 +5215,36 @@ window.addEventListener('shown.bs.modal', function (event) {
 // Hide navbar on scroll down.
 var prevScrollPos = window.scrollY;
 var navbar = document.getElementById('bottom-nav');
+var secondaryNavbar = document.getElementById('secondary-nav');
 
 // Function to check the screen width and apply the style accordingly.
 var setNavOffset = function setNavOffset() {
   if (window.matchMedia("(max-width: 768px)").matches) {
     navbar.style.bottom = '1rem';
   } else {
+    navbar.classList.toggle('pt-1', false);
     navbar.style.bottom = '2rem';
   }
+  navbar.style.cursor = 'default';
 };
 window.onscroll = function () {
   var currentScrollPos = window.scrollY;
   if (prevScrollPos > currentScrollPos) {
     setNavOffset();
   } else {
-    navbar.style.bottom = '-90px';
+    navbar.style.cursor = 'pointer';
+    if (navbar.contains(secondaryNavbar)) {
+      navbar.style.bottom = '-90px';
+    } else {
+      navbar.classList.toggle('pt-1', true);
+      navbar.style.bottom = '-55px';
+    }
   }
   prevScrollPos = currentScrollPos;
 };
+navbar.addEventListener('click', function (e) {
+  setNavOffset();
+});
 
 /***/ }),
 
--- resources/js/libs/nav.js
+++ resources/js/libs/nav.js
@@ -2,23 +2,37 @@
 // Hide navbar on scroll down.
 let prevScrollPos = window.scrollY;
 const navbar = document.getElementById('bottom-nav');
+const secondaryNavbar = document.getElementById('secondary-nav');
 
 // Function to check the screen width and apply the style accordingly.
 const setNavOffset = () => {
     if (window.matchMedia("(max-width: 768px)").matches) {
         navbar.style.bottom = '1rem';
     } else {
+        navbar.classList.toggle('pt-1', false);
         navbar.style.bottom = '2rem';
     }
+    navbar.style.cursor = 'default';
 }
 
 window.onscroll = function() {
     const currentScrollPos = window.scrollY;
+    
     if (prevScrollPos > currentScrollPos) {
         setNavOffset();
     } else {
-        navbar.style.bottom = '-90px';
+        navbar.style.cursor = 'pointer';
+        if (navbar.contains(secondaryNavbar)) {
+            navbar.style.bottom = '-90px';
+        } else {
+            navbar.classList.toggle('pt-1', true);
+            navbar.style.bottom = '-55px';
+        }
     }
 
     prevScrollPos = currentScrollPos;
 }
+
+navbar.addEventListener('click', (e) => {
+    setNavOffset();
+})
