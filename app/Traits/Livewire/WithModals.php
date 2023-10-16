49cfe6fe3a412afa2e5492f6feed54df03fef504
J-Pard <pardons.jonathan@gmail.com>
2023-10-16 10:20:59 +0200

    Trait WithModals

---
+++ app/Traits/Livewire/WithModals.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits\Livewire;
+
+trait WithModals {
+
+    /**
+     * Show modal with specified id.
+     *
+     * @param string $modalId
+     * @return void
+     */
+    protected function showModal(string $modalId)
+    {
+        $this->dispatch('form-modal-loaded', modalId: $modalId);
+    }
+
+    /**
+     * Hide modal with specified id.
+     *
+     * @param string $modalId
+     * @return void
+     */
+    protected function hideModal(string $modalId)
+    {
+        $this->dispatch('form-modal-saved', modalId: $modalId);
+    }
+}
